23a95f469cca97b27c9f9f6b968a9b55363bbe9c
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-03-02 14:07:25 +0530

    Create m.c

---
+++ m.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[30];
+int len;
+printf("Enter the string:");
+gets(str);
+len=strlen(str);
+printf("The value is %d",len);
+     
+}
+getch();
+}
